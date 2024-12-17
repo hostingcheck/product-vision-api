03b4af5c50b18cb23c997123fc75952b1c9c9354
Anshj07 <103409932+Ansh071200@users.noreply.github.com>
2024-12-17 22:49:45 +0530

    feat: added cross origin requests

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const mongoURI = process.env.MONGODB_URI;
 const updatedURI = mongoURI.replace('mongodb://', 'mongodb+srv://').split(',')[0];
